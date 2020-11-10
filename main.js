c1ec3ba73b5fb1b11e0a94ed62400ed69fe481aa
Karina Doskuch <karina.doskuch@gmail.com>
2020-11-10 17:14:00 +0200

    Work on the bugs.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ console.log(wordsList(myStr, 'try'));
 
 //2
 function getLocalDate(date, isSeconds = false, isISO = false) {
-    var date = new Date(date);
+    // var date = new Date(date);
     if (isISO === true) {
         return toISO(date, isSeconds);
     }
@@ -27,82 +27,77 @@ function getLocalDate(date, isSeconds = false, isISO = false) {
 }
 function toISO(date, isSeconds) {
     var result = date.getUTCFullYear() +
-      '-' + pad(date.getUTCMonth()) +
-      '-' + pad(date.getUTCDate()) +
-      ', ' + pad(date.getUTCHours()) +
-      ':' + pad(date.getUTCMinutes());
+        '-' + pad(date.getUTCMonth() + 1) +
+        '-' + pad(date.getUTCDate()) +
+        ', ' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes());
       return isSeconds === true ? result + ':' + pad(date.getUTCSeconds()) : result;
 };
 
 function toDate(date, isSeconds) {
-    var result = date.getFullYear() +
-      '/' + pad(date.getMonth()) +
-      '/' + pad(date.getDate()) +
-      ', ' + pad(date.getHours()) +
-      ':' + pad(date.getMinutes());
-      return isSeconds === true ? result + ':' + pad(date.getSeconds()) : result;
+    var result = pad(date.getDate()) +
+        '.' + pad(date.getMonth() + 1) +
+        '.' + date.getFullYear() +
+        ', ' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes());
+    return isSeconds === true ? result + ':' + pad(date.getSeconds()) : result;
 };
 
 function pad(number) {
     return number <= 9 ? `0${number}`.slice(-2) : number;
 }
 
-console.log(getLocalDate("2020-11-15 14:33:22"));
-console.log(getLocalDate("2020-11-15 14:33:22", true));
-console.log(getLocalDate("2020-11-15 14:33:22", false, true));
-console.log(getLocalDate("2020-11-15 14:33:22", true, true));
+console.log(getLocalDate(new Date("2020-11-15 14:33:22")));
+console.log(getLocalDate(new Date("2020-11-15 14:33:22"), true));
+console.log(getLocalDate(new Date("2020-11-15 14:33:22"), false, true));
+console.log(getLocalDate(new Date("2020-11-15 14:33:22"), true, true));
 
 //3
 function getWeekDay(date) {
    var date = new Date(date);
    var days = [
+    'воскресенье',
     'понедельник',
     'вторник',
     'среда',
     'четверг',
     'пятница',
-    'суббота',
-    'воскресенье'
+    'суббота'
     ];
     return days[date.getDay()];
 }
-console.log(getWeekDay('2020-11-07'));
+console.log(getWeekDay('2019-01-30'));
+console.log(getWeekDay('2020-11-09'));
 console.log(getWeekDay('2020-11-27'));
 
 //4
-
- function getLocalDay(date) {
+function getLocalDay(date) {
     var date = new Date(date);
     var day = date.getDay();
-    if (day === 0) {
-    day = 7;
-    } 
-  
+    if (day === 0) day = 7;
     return day;
-  };
-  console.log(getLocalDay('2020-11-25'));
-  console.log(getLocalDay('2020-12-31'));
+};
+console.log(getLocalDay('2020-11-25'));
+console.log(getLocalDay('2020-12-31'));
 
-  //5
-
-  function getDateAgo(date, days) {
+//5
+function getDateAgo(date, days) {
    var date = new Date(date);
   
     date.setDate(date.getDate() - days);
-    return date.toLocaleString().replace(/(\d.*),\s+(\d.*)/g, '$1');
-  }
-  console.log(getDateAgo('2020-11-29', 7));
-  console.log(getDateAgo('2020-12-31', 1));
+    return pad(date.getDate()) + '.' + pad(date.getMonth() + 1)+ '.' + date.getFullYear();
+};
+console.log(getDateAgo('2019-01-29', 1));
+console.log(getDateAgo('2020-12-31', 1));
  
 //6
-
 var Car = function (engine, model, name, year) {
     this.engine = engine;
     this.model = model;
     this.name = name;
     this.year = year;
-  };
-  Object.defineProperties(Car.prototype, {
+};
+Object.defineProperties(Car.prototype, {
     used: {
      get() {
     var yearNow = new Date().getFullYear();
@@ -113,48 +108,48 @@ var Car = function (engine, model, name, year) {
         if (value === 'new' && this.year < yearNow) this.year = yearNow;
       }
     }
-  });
-  Car.prototype.info = function () {
+});
+Car.prototype.info = function () {
     return `${this.name} ${this.model}, ${this.engine}cc, year ${this.year}, ${this.used}`;
-  };
+};
   
-    var car = new Car(2001, 'X6', 'BMV', 2020);
-    var car2 = new Car(1900, 'Actavia', 'Shkoda', 2016);
+var car = new Car(2001, 'X6', 'BMV', 2020);
+var car2 = new Car(1900, 'Actavia', 'Shkoda', 2016);
 
-  console.log(car.info()); 
-  car.used = 'new';
-  console.log(car.info());
-  car.used = 'used';
-  console.log(car.info());
-  console.log(car2.info()); 
-  car.used = 'used';
-  console.log(car2.info());
+console.log(car.info()); 
+car.used = 'new';
+console.log(car.info());
+car.used = 'used';
+console.log(car.info());
+console.log(car2.info()); 
+car.used = 'used';
+console.log(car2.info());
 
-  //7
-
-  function testPerformance(iterations, func) {
+//7
+function testPerformance(iterations, func) {
     var time = Date.now();
     if (typeof func === 'function') {
-    for (let i = iterations; i--;) func();
-    
-    return Date.now() - time;
-    } 
-  };
-  function test() {
+        for (var i = iterations; i >= 0; i--) {
+            func();
+        }
+    }
+    return Date.now() - time; 
+};
+function test1() {
     var str = myStr;
-     str = str.replace('c', '');
-     str = str.replace('o', '');
-     str = str.replace('n', '');
-     str = str.replace('t', '');
-     str = str.replace('r', '');
-     str = str.replace('y', '');
-  }
+    while (str.indexOf('c') !== -1) str = str.replace('c', '');
+    while (str.indexOf('o') !== -1) str = str.replace('o', '');
+    while (str.indexOf('u') !== -1) str = str.replace('u', '');
+    while (str.indexOf('n') !== -1) str = str.replace('n', '');
+    while (str.indexOf('t') !== -1) str = str.replace('t', '');
+    while (str.indexOf('r') !== -1) str = str.replace('r', '');
+    while (str.indexOf('y') !== -1) str = str.replace('y', '');
+}
   
-  function test1() {
+function test2() {
     var re = new RegExp('[country]', 'g');
-  
     myStr.replace(re, '');
-  }
-  console.log(testPerformance(20, test));
-  console.log(testPerformance(80, test1));
-  console.log(testPerformance(20, ));
+}
+console.log(testPerformance(20, test1));
+console.log(testPerformance(80, test2));
+console.log(testPerformance(20, 2222));
